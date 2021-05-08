fb073eb79cb27000dfca47031619cff9ce10fa42
Victor Ryadovoi <r.v.i.c@yandex.ru>
2021-05-09 01:32:19 +0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Victor Ryadovoi on 08.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
